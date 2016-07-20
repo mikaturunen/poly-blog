bb34da19abf27bac95187f527a3038a0f3aca06d
Travis CI <mika.turunen@ymail.com>
2016-07-20 08:08:39 +0000

    Deploy to GitHub Pages: 76fa20818d3eef57ac285e61be50a6989f276c55

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,1 +1,1 @@
-module.exports={staticFileGlobs:["/poly-blog/index.html","/poly-blog/manifest.json","/poly-blog/bower_components/webcomponentsjs/webcomponents-lite.min.js"],navigateFallback:"./index.html"};
+module.exports={staticFileGlobs:["/poly-blog/index.html","/poly-blog/manifest.json","/poly-blog/bower_components/webcomponentsjs/webcomponents-lite.min.js"],navigateFallback:"/poly-blog/index.html"};
